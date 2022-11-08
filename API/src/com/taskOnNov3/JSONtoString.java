b725a9fc6a459e82a0be856d29f718ba02067f6f
Mugila kanagaraj <81410764+Mugilak@users.noreply.github.com>
2022-11-08 09:26:00 +0530

    commit

--- API/src/com/taskOnNov3/JSONtoString.java
+++ API/src/com/taskOnNov3/JSONtoString.java
@@ -1,11 +1,36 @@
 package com.taskOnNov3;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import model.Data;
+import model.Details;
+import model.Support;
+import swing.Swing;
+
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 import java.net.HttpURLConnection;
 import java.io.*;
 
 public class JSONtoString {
+	private List<Details> details;
+	private Support support;
+	private Details detail;
+	private Swing swing;
+
+	public JSONtoString() {
+		support = new Support();
+	}
 
 	public static void main(String[] args) {
 		JSONtoString object = new JSONtoString();
@@ -17,16 +42,26 @@ public class JSONtoString {
 			URL url = new URL("https://reqres.in/api/unknown");
 			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 			BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-			String line;
-			while ((line = reader.readLine()) != null) {
-				System.out.println(line);
+			String jsonResponse;
+			StringBuilder sb = new StringBuilder();
+			while ((jsonResponse = reader.readLine()) != null) {
+				sb.append(jsonResponse);
 			}
+			System.out.println(sb.toString());
+			detail = JSONtoPojo(sb.toString());
+			swing = new Swing(detail);
+			swing.addInJList(swing);
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
 
+	private Details JSONtoPojo(String jsonData) throws JsonMappingException, JsonProcessingException, ParseException {
+		ObjectMapper mapper = new ObjectMapper();
+		Details detail = mapper.readValue(jsonData, Details.class);
+		return detail;
 	}
 
 }
---
+++ API/src/swing/Swing.java
@@ -0,0 +1,81 @@
+package swing;
+
+import javax.swing.*;
+import javax.swing.event.*;
+
+import model.Data;
+import model.Details;
+
+public class Swing extends JFrame implements ListSelectionListener {
+	private JList a, b;
+	private JLabel label1, ans;
+	private Details detail;
+	private Swing swing;
+	private JFrame frame;
+	private JPanel panel;
+	private String answer;
+
+	public Swing(Details details) {
+		detail = details;
+	}
+
+	public void addInJList(Swing swing) {
+		answer = "";
+		frame = new JFrame();
+		panel = new JPanel();
+		frame.setSize(600, 600);
+		label1 = new JLabel("Select field : ");
+		int i = 0;
+		String[] array = { "page", "per page", "total", "total pages", "Data", "Support" };
+		a = new JList(array);
+		ans = new JLabel();
+		String[] data = new String[detail.getData().size()];
+		for (Data d : detail.getData()) {
+			answer =d.getId() + " \n" + d.getName() + " " + d.getColor() + " " + d.getYear() + " "
+					+ d.getPantone_value();
+			data[i++] = answer;
+		}
+		b=new JList(data);
+		b.setVisible(false);
+		a.setSelectedIndex(0);
+		a.addListSelectionListener(swing);
+		panel.add(label1);
+		panel.add(a);
+		panel.add(b);
+		panel.add(ans);
+
+		frame.add(panel);
+
+		frame.show();
+	}
+
+	@Override
+	public void valueChanged(ListSelectionEvent e) {
+		switch (a.getSelectedValue().toString()) {
+		case "page":
+			b.setVisible(false);
+			ans.setText(((Integer) detail.getPage()).toString());
+			break;
+		case "per page":
+			b.setVisible(false);
+			ans.setText(((Integer) detail.getPer_page()).toString());
+			break;
+		case "total":
+			b.setVisible(false);
+			ans.setText(((Integer) detail.getTotal()).toString());
+			break;
+		case "total pages":
+			b.setVisible(false);
+			ans.setText(((Integer) detail.getTotal_pages()).toString());
+			break;
+		case "Data":
+			b.setVisible(true);
+			break;
+		case "Support":
+			b.setVisible(false);
+			ans.setText(detail.getSupport().getText() + " \n" + detail.getSupport().getUrl());
+			break;
+		}
+
+	}
+}
